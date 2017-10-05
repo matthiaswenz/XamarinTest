b5bf280dd835931436cd4ec03e110cf3fedb8956
Matthias Wenz <matwen@microsoft.com>
2017-10-05 10:16:16 -0700

    Many many changes

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using HockeyApp.Android;
+using HockeyApp.Android.Metrics;
+
 using TestLibrary;
 
 using System.Security.Cryptography;
@@ -19,14 +22,24 @@ namespace MatXiTest.Droid
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
+			CrashManager.Register(this, "59a2597b38d54127be846d91b1e5d6a4");
+			MetricsManager.Register(Application, "59a2597b38d54127be846d91b1e5d6a4");
+
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.myButton);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
             ECCurve curve = ECCurve.CreateFromValue("");
 
             Class1 cls = new Class1();
+
+			button.Click += delegate { 
+				button.Text = string.Format("{0} clicks!", count++);
+
+				MetricsManager.TrackEvent("button_clicked");
+
+				throw new System.Exception();
+			};
 		}
 	}
 }
